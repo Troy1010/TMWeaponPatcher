30f4999777b23afa04dc80b7b566f56de27e5632
Troy <troyswanson1010@gmail.com>
2023-06-30 00:59:05 -0500

    start, weapon speeds patched

---
+++ TMWeaponPatcher/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace TMWeaponPatcher;
+
+public class Settings
+{
+    [MaintainOrder]
+
+    [SettingName("Speed Multiplier")]
+    [Tooltip("Multiplies each weapon's speed by this value.")]
+    public float SpeedMultiplier = 0.75f;
+}
